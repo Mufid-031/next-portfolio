2c89415e0285ab89c6471cbc00f23fdbb3d44791
Mufid-031 <mufidmaxsus17@gmail.com>
2024-08-21 16:24:16 +0700

    Sempat terjadi error setelah push dan sekarang udh ok

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,20 +12,21 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      animation: {
-        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
-      },
-      keyframes: {
-        scroll: {
-          to: {
-            transform: "translate(calc(-50% - 0.5rem))",
-          },
-        },
-      }
+      // animation: {
+      //   scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      // },
+      // keyframes: {
+      //   scroll: {
+      //     to: {
+      //       transform: "translate(calc(-50% - 0.5rem))",
+      //     },
+      //   },
+      // },
     },
   },
   plugins: [addVariablesForColors, require("@tailwindcss/typography")],
 };
+
 export default config;
 
 function addVariablesForColors({ addBase, theme }: any) {
