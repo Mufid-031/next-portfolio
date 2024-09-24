bba4a518ebce1b5dae83c57cad53715fcd04c434
Mufid-031 <mufidmaxsus17@gmail.com>
2024-09-24 16:06:29 +0700

    mengubah file next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  turbo: {
-    enable: true,
-  },
-
   images: {
     domains: ["images.unsplash.com"],
   },
